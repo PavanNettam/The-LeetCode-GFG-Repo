e0edf9a5b0d4691fa17add92b2b87991a5498418
PavanNettam <79460453+PavanNettam@users.noreply.github.com>
2022-05-01 15:13:48 +0530

    Time: 19 ms (6.96%), Space: 6.5 MB (6.98%) - LeetHub

---
+++ 844-backspace-string-compare/844-backspace-string-compare.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        stack<char> s1;
+        stack<char> t1;
+        int i=0;
+        while(s[i] != '\0'){
+            if(s[i] == '#' && !s1.empty()){
+                cout<<"s1.pop()="<<s1.top()<<endl;
+                s1.pop();
+            }else if(s[i]!='#'){
+                s1.push(s[i]);
+            }
+            i++;
+            
+        }
+        i = 0 ;
+        while(t[i] != '\0'){
+            if(t[i] == '#' && !t1.empty()){
+                cout<<"t1.pop()="<<t1.top()<<endl;
+                t1.pop();
+            }else if(t[i]!='#') {
+                t1.push(t[i]);
+            }
+            i++;
+            
+        }
+        while(!s1.empty() && !t1.empty()){
+            if(s1.top() != t1.top()) return 0;
+            s1.pop();
+            t1.pop();
+        }
+        if(s1.empty() && t1.empty())
+            return 1;
+        else
+            return 0;
+    }
+};
