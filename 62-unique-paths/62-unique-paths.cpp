c962defa71ff397b250788fbcfa625bd80b4dd97
PavanNettam <79460453+PavanNettam@users.noreply.github.com>
2022-05-02 07:23:09 +0530

    Time: 0 ms (100.00%), Space: 5.7 MB (99.99%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,12 +1,9 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        if(m==1 && n==1) return 1; //If only one cell then only one path.
+        if(m==1 && n==1) return 1; 
         
-        int arr[m][n]; //dp table
-        
-        //Making 0th row and column elements as 1 as there is only one path
-        //from 0,0 to any of these cells
+        int arr[m][n]; 
         
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
@@ -14,13 +11,7 @@ public:
                 else if(j == 0) arr[i][j] = 1;
                 else arr[i][j] = 0;
             }
-        }
-        
-        //A particular cell can be reached in only two possible ways 
-        //1. From the cell just above it
-        //or 
-        //2. From the cell just left to it
-        // so each of these ways can be considered as different pahts: 
+        } 
         
         for(int i=1;i<m;i++){
             for(int j=1;j<n;j++){
