b3b61f3e60e897c48ed7121e6022c0c9a0fa73ef
PavanNettam <79460453+PavanNettam@users.noreply.github.com>
2022-05-31 20:12:09 +0530

    Time: 83 ms (5.67%), Space: 12.2 MB (37.76%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,28 +1,46 @@
 class Solution {
 public:
+    
     void nextPermutation(vector<int>& nums) {
-        int i = nums.size()-2;
-        while( i >= 0 && nums[i] >= nums[i+1]){
+        int n = nums.size();
+        if(n==1) return;
+        int flag = 0;
+        for(int i=0;i<n-1;i++){
+            if(nums[i]<nums[i+1]){
+                flag = 1;
+            }
+        }
+        for(int i=0;i<n;i++){
+            cout<<"c1"<<endl;
+            cout<<nums[i]<<endl;
+        }
+        if(flag == 0){
+            reverse(nums.begin(),nums.end());
+            return;
+        }
+        int i=n-2;
+        while(nums[i+1]<=nums[i]){
             i--;
         }
-        if(i >= 0){
-            int j = nums.size()-1;
-            while(nums[j]  <= nums[i]){
-                j--;
-            }
-            int temp = nums[j];
-            nums[j] = nums[i];
-            nums[i] = temp;
+        for(int i=0;i<n;i++){
+            cout<<"c2"<<endl;
+            cout<<nums[i]<<endl;
         }
-        int l = i+1;
-        int r = nums.size()-1;
-        //cout<<l<<" "<<r<<endl;
-        while(l<r){
-            int temp = nums[l];
-            nums[l] = nums[r];
-            nums[r] = temp;
-            l++;
-            r--;
+        int j = n-1;
+        while(nums[j]<=nums[i]){
+            j--;
+        }
+        for(int i=0;i<n;i++){
+            cout<<"c3"<<endl;
+            cout<<nums[i]<<" "<<j<<endl;
+        }
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+        reverse(nums.begin()+i+1,nums.end());
+        for(int i=0;i<n;i++){
+            cout<<"c4"<<endl;
+            cout<<nums[i]<<endl;
         }
     }
 };
