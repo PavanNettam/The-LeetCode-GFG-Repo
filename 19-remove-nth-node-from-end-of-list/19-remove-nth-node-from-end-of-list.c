cf4d59c21e27b59af82e762ff34f0772a07cfd9e
PavanNettam <79460453+PavanNettam@users.noreply.github.com>
2022-06-30 10:14:43 +0530

    Time: 3 ms (65.56%), Space: 5.6 MB (98.10%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.c
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.c
@@ -9,32 +9,19 @@
 
 struct ListNode* removeNthFromEnd(struct ListNode* head, int n){
     struct ListNode* p = head;
-    if(head == NULL){
-        return;
+    struct ListNode* q = head;
+    if(head->next == NULL) return head->next;
+    while(n>0){
+        p = p->next;
+        n--;
     }
-    int count = 0;
-    while(p!=NULL){
-        count++;
+    if(p == NULL){
+        return head->next;
+    }
+    while(p->next != NULL){
+        q = q->next;
         p = p->next;
     }
-    //printf("%d",count);
-    if(n>count){
-        return head;
-    }
-    int des = count - n;
-    p = head;
-    struct ListNode* q = NULL;
-    for(int i=1;i<=des;i++){
-        q = p;
-        p = p->next;
-    }
-    if(q == NULL){
-        head = p->next;
-        free(p);
-        return head;
-    }else{
-        q->next = p->next;
-        free(p);
-    }
+    q->next = q->next->next;
     return head;
 }
