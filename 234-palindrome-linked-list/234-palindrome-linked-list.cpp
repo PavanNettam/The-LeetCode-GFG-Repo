fd48c6516951b9956fffa82c461cc67b16c3c124
PavanNettam <79460453+PavanNettam@users.noreply.github.com>
2022-07-01 18:15:13 +0530

    Time: 550 ms (5.69%), Space: 118 MB (48.81%) - LeetHub

---
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        if(head->next == NULL) return 1;
+        ListNode* p = head;
+        ListNode* q = head;
+        ListNode* r = NULL;
+        do{
+            if(p->next!= NULL) p = p->next->next;
+            r = q;
+            q = q->next;
+        }while(p!=NULL && p->next != NULL);
+        //cout<<(q->val)<<endl;
+        if(p!=NULL){
+            r->next = q->next;
+            q = r->next;
+        }
+        ListNode* note = r;
+        r = NULL;
+        p = q->next;
+        while(q!=NULL){
+            q->next = r;
+            r = q;
+            q = p;
+            if(p!=NULL) p = p->next;
+        }
+        note->next = r;
+        p = head;
+        // ListNode* x = head;
+        // while(x!=NULL) cout<<(x->val)<<endl;
+        while(r!=NULL){
+            if(p->val != r->val) return 0;
+            p = p->next;
+            r = r->next;
+        }
+        return 1;
+    }
+};
