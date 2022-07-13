ab32364a7446836753210411690946cb4ba47e62
PavanNettam <79460453+PavanNettam@users.noreply.github.com>
2022-07-13 11:25:37 +0530

    Time: 57 ms (14.52%), Space: 27.7 MB (41.01%) - LeetHub

--- 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.cpp
+++ 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.cpp
@@ -13,12 +13,12 @@ class Solution {
 public:
     int findPath(TreeNode* root, int& maxi){
         if(root == NULL) return 0;
-        
+        //If u get a zero from left or right path then dont consider it.
         int l = max(0,findPath(root->left,maxi));
         int r = max(0,findPath(root->right,maxi));
-        
+        //Maximum 'U' shaped path
         maxi = max(maxi,l + r + root->val);
-        
+        //Return the max branch.
         return max(l,r) + root->val;
     }
     int maxPathSum(TreeNode* root) {
