233af51a9e51f6cdc9f998dca7111da7afbb0c80
PavanNettam <79460453+PavanNettam@users.noreply.github.com>
2022-08-03 07:51:49 +0530

    Time: 115 ms (5.03%), Space: 38.4 MB (5.00%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
-    int bfsColor(int curr,vector<vector<int>> graph, vector<int>& color){
-        queue<int> q;
-        q.push(curr);
-        color[curr] = 0;
-        while(!q.empty()){
-            int ele = q.front();
-            q.pop();
-            for(int i=0;i<graph[ele].size();i++){
-                if(color[graph[ele][i]] == -1){
-                    color[graph[ele][i]] = (color[ele]==0)?1:0;
-                    q.push(graph[ele][i]);
-                }else{
-                    if(color[graph[ele][i]] == color[ele]){
-                        return 0;
-                    }
+    int dfsColor(vector<vector<int>> graph,vector<int>& color,int curPos,int curCol){
+        color[curPos] = curCol;
+        for(auto i:graph[curPos]){
+            if(color[i] == -1){
+                int nextCol = (curCol==0)?1:0;
+                if (!dfsColor(graph,color,i,nextCol)){
+                    return 0;
+                }
+            }else{
+                if(curCol == color[i]){
+                    return 0;
                 }
             }
         }
@@ -25,7 +21,7 @@ public:
         vector<int> color(n,-1);
         for(int i=0;i<n;i++){
             if(color[i] == -1){
-                if(!bfsColor(i,graph,color)){
+                if(!dfsColor(graph,color,i,0)){
                     return 0;
                 }
             }
@@ -33,10 +29,3 @@ public:
         return 1;
     }
 };
-
-/*
-Performed a bfs:
-Idea is to color the graph is opposite colors for adjacent nodes, if we once we visit a node we check all its adjacent nodes and see if they are colored.
-(1) If they are not colored then color oppositely 
-(2) else check the already existing color if its not opposite then reutrn false.
-*/
