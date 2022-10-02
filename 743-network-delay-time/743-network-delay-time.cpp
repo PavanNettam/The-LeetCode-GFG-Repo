6375120657f8cc464c2138e0af733fb590a71de3
PavanNettam <79460453+PavanNettam@users.noreply.github.com>
2022-10-02 19:07:54 +0530

    Time: 467 ms (8.09%), Space: 37.8 MB (93.95%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -1,54 +1,52 @@
 class Solution {
 public:
-    vector<pair<int, int>> adj[101];
-    
-    void dijkstra(vector<int>& signalReceivedAt, int source, int n) {
-        priority_queue<pair<int, int>, vector<pair<int, int>>, 
-        greater<pair<int, int>>> pq;
-        pq.push({0, source});
-    
-        signalReceivedAt[source] = 0;
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
         
-        while (!pq.empty()) {
-            int currNodeTime = pq.top().first;
-            int currNode = pq.top().second; 
-            pq.pop();
-            
-            if (currNodeTime > signalReceivedAt[currNode]) {
-                continue;
-            }
-            
-            for (pair<int, int> edge : adj[currNode]) {
-                int time = edge.first;
-                int neighborNode = edge.second;
-            
-                if (signalReceivedAt[neighborNode] > currNodeTime + time) {
-                    signalReceivedAt[neighborNode] = currNodeTime + time;
-                    pq.push({signalReceivedAt[neighborNode], neighborNode});
+        vector<vector<int>> v( n+1 , vector<int> (n+1, 6001)),temp; 
+        
+        for(int i=0;i<times.size();i++){
+            v[times[i][0]][times[i][1]] = times[i][2];
+        }
+        for(int i=1;i<=n;i++){
+            v[i][i] = 0;
+        
+        }
+        temp = v;
+        // cout<<0<<":"<<endl;
+        //     for(int i=1;i<=n;i++){
+        //         for(int j=1;j<=n;j++){
+        //             cout<<v[i][j]<<" ";
+        //         }
+        //         cout<<endl;
+        //     }
+        for(int x=1;x<=n;x++){
+            for(int i=1;i<=n;i++){
+                for(int j=1;j<=n;j++){
+                    if(v[i][x]+v[x][j] < v[i][j]){
+                        temp[i][j] = v[i][x]+v[x][j];
+                    }
                 }
             }
-        }
-    }
-    
-    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-      
-        for (vector<int> time : times) {
-            int source = time[0];
-            int dest = time[1];
-            int travelTime = time[2];
             
-            adj[source].push_back({travelTime, dest});
+            v = temp;
+            // cout<<x<<":"<<endl;
+            // for(int i=1;i<=n;i++){
+            //     for(int j=1;j<=n;j++){
+            //         cout<<v[i][j]<<" ";
+            //     }
+            //     cout<<endl;
+            // }
         }
         
-        vector<int> signalReceivedAt(n + 1, INT_MAX);
-        dijkstra(signalReceivedAt, k, n);
-        
-        int answer = INT_MIN;
-        for (int i = 1; i <= n; i++) {
-            answer = max(answer, signalReceivedAt[i]);
+    
+        int maxi = v[k][1];
+        for(int i=1;i<=n;i++){
+            maxi = max(maxi,v[k][i]);
+        }
+        if(maxi >= 6001){
+            return -1;
         }
         
-        
-        return answer == INT_MAX ? -1 : answer;
+        return maxi;
     }
 };
