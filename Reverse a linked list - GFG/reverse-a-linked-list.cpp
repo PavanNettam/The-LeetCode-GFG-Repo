751b79849e07ceae7702a3d3bffa9b6c9bcf0619
PavanNettam <79460453+PavanNettam@users.noreply.github.com>
2023-05-31 06:00:45 +0530

    Added solution - LeetHub

---
+++ Reverse a linked list - GFG/reverse-a-linked-list.cpp
@@ -0,0 +1,99 @@
+//{ Driver Code Starts
+//Initial Template for C++// C program to find n'th Node in linked list
+#include <stdio.h>
+#include <stdlib.h>
+#include<iostream>
+using namespace std;
+
+/* Link list Node */
+struct Node {
+    int data;
+    struct Node *next;
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+
+
+// } Driver Code Ends
+/* Linked List Node structure:
+
+struct Node
+{
+    int data;
+    struct Node *next;
+}
+
+*/
+
+class Solution
+{
+    public:
+    //Function to reverse a linked list.
+    struct Node* reverseList(struct Node *head)
+    {
+        Node* r = NULL;
+        Node* q = head;
+        Node* p = q->next;
+        while(q!=NULL){
+            q->next = r;
+            r = q;
+            q = p;
+            p = (p!=NULL)?p->next:NULL;
+        }
+        return r;
+    }
+    
+};
+    
+
+
+//{ Driver Code Starts.
+
+void printList(struct Node *head)
+{
+    struct Node *temp = head;
+    while (temp != NULL)
+    {
+       printf("%d ", temp->data);
+       temp  = temp->next;
+    }
+}
+
+/* Driver program to test above function*/
+int main()
+{
+    int T,n,l,firstdata;
+    cin>>T;
+
+    while(T--)
+    {
+        struct Node *head = NULL,  *tail = NULL;
+
+        cin>>n;
+        
+        cin>>firstdata;
+        head = new Node(firstdata);
+        tail = head;
+        
+        for (int i=1; i<n; i++)
+        {
+            cin>>l;
+            tail->next = new Node(l);
+            tail = tail->next;
+        }
+        
+        Solution ob;
+        head = ob. reverseList(head);
+        
+        printList(head);
+        cout << endl;
+    }
+    return 0;
+}
+
+
+// } Driver Code Ends
