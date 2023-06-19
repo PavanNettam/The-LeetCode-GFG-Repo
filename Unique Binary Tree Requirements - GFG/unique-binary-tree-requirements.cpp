2c653e38532652fcef76a95fc9d06d1f61fa8a45
PavanNettam <79460453+PavanNettam@users.noreply.github.com>
2023-06-19 16:07:43 +0530

    Added solution - LeetHub

---
+++ Unique Binary Tree Requirements - GFG/unique-binary-tree-requirements.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+public:
+    bool isPossible(int a,int b)
+    {
+        if(a == b) return 0;
+        if(a == 2 || b == 2) return 1;
+        return 0;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a,b; 
+        cin>>a>>b;
+        Solution obj;
+        bool ans=obj.isPossible(a,b);
+        cout<<ans<<"\n";
+    }
+}
+// } Driver Code Ends
